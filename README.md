9a9fd999bad5847e34c195edbba22fcfc62ceb86
Flemming Stark <flemming@starkware.co>
2014-05-26 00:35:33 -0400

    late because of medical conditions

---
+++ README.md
@@ -0,0 +1,11 @@
+#Getting and Cleaning Data course project
+
+The main script is run_analysis.R  
+This script works with the provided datasets to generete the final tidy dataset (provided here for reference)  
+The proccess of this script is the following:
+
+* Merges the training and the test sets to create one data set.
+* Extracts only the measurements on the mean and standard deviation for each measurement. 
+* Uses descriptive activity names to name the activities in the data set
+* Appropriately labels the data set with descriptive activity names. 
+* Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
--- run_analysis.R
+++ run_analysis.R
@@ -1,38 +1,45 @@
-require(reshape2)
+#set working directory to the repository directory
+#the data to work with is provided inside the repository
+setwd(".")
 
-setwd("~/Study/Coursera/Getting and Cleaning Data/tidydata")
 
-
+#get test datasets
 subject_test<-read.table("./UCI HAR Dataset/test/subject_test.txt")
 x_test<-read.table("./UCI HAR Dataset/test/X_test.txt")
 y_test<-read.table("./UCI HAR Dataset/test/y_test.txt")
 
 
-
+#get training datasets
 subject_train<-read.table("./UCI HAR Dataset/train/subject_train.txt")
 x_train<-read.table("./UCI HAR Dataset/train/X_train.txt")
 y_train<-read.table("./UCI HAR Dataset/train/y_train.txt")
 
 
+#combine test and training to get final datasets
 subject_final<-rbind(subject_test,subject_train)
 x_final<-rbind(x_test,x_train)
 y_final<-rbind(y_test,y_train)
 
+
+#setting column names 
 colnames(subject_final)<-"Subject"
 colnames(y_final)<-"Activity.id"
-
 features<-read.table("./UCI HAR Dataset/features.txt")
 colnames(features)<-c("Feature.id","Feature.name")
-
 colnames(x_final)<-features$Feature.name
 
+
+#binding independent parts to create data set
 tidy_dataset<-cbind(subject_final,y_final,x_final)
 
+#selecting the columns containing the mean and the std only
 tidy_dataset.selected<-grep("-mean\\(\\)|-std\\(\\)", colnames(tidy_dataset))
 tidy_dataset<-tidy_dataset[,c(1,2,tidy_dataset.selected)]
 
+#setting activities labels
 activity_labels<-read.table("./UCI HAR Dataset/activity_labels.txt", stringsAsFactors=FALSE)
 colnames(activity_labels)<-c("Activity.id","Activity.name")
 tidy_dataset<-merge(tidy_dataset, activity_labels, by="Activity.id")
 
+#writing dataset to file
 write.table(tidy_dataset, file = "./TidyData.csv", sep = " ", row.names = FALSE, col.names = TRUE)
